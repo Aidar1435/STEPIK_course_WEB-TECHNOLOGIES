072dc9adbbcd1175ae524646c9252e66580f9247
grmsoft <grmsoft@mail.ru>
2022-02-01 00:29:19 +0000

    Update ./init.sh (add gunicorn command launch)

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+
+# nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# gunicorn
+sudo gunicorn -b "0.0.0.0:8080" hello:print_query &
