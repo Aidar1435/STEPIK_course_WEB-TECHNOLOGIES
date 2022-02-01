9a634315843e68391ef61c7d56e3c3fcb970bf54
grmsoft <grmsoft@mail.ru>
2022-02-01 01:22:03 +0000

    Modify ./hello.py after debug and tests

--- hello.py
+++ hello.py
@@ -5,5 +5,5 @@ study project
 
 def print_query(environ, start_response):
     '''print query string. each parameter on a new line'''
-    start_responce('200 OK', [('Content-type', 'text/plain')])
-    yield '\n'.join(environ['QUERY_STRING'].split('&')).decode('utf-8')
+    start_response('200 OK', [('Content-type', 'text/plain')])
+    yield environ['QUERY_STRING'].replace('&', '\n').encode('utf-8')
