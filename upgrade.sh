f1f1fcb809eab4e8626b66ec397cf6010fa45bf0
grmsoft <grmsoft@mail.ru>
2022-02-01 14:24:29 +0300

    Fix ./upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,8 +1,12 @@
-#!bin/bash
+#!/bin/bash
 sudo apt-get update
-sudo apt-get install -y python3.5 python3.5-dev
+sudo apt-get install -y python3.5
 sudo unlink /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+curl "https://bootstrap.pypa.io/pip/3.5/get-pip.py" > get-pip.py
+sudo python3 get-pip.py
+rm get-pip.py
+sudo apt-get install -y python3.5-dev
 sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade django==2.1
 sudo pip3 install --upgrade gunicorn
