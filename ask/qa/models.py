53d1863b8c4c2774f14fd21aa1b7de3b1d58d91b
grmsoft <grmsoft@mail.ru>
2022-02-02 20:11:59 +0300

    Update ask/qa/models.py according to the task

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
+
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    addet_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerInt(default=0)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.title
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return super(QuestionManager, self).get_queryset().order_by('-added_at')
+    def popular(self):
+        return super(QuestionManager, self).get_queryset().annotate(models.Count('likes')).order_by('likes__count')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    addet_at = models.DateTimeField(auto_now_add=True)
+
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
 # Create your models here.
