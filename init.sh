d389d8974adbf67ad3b8c78c1810116bfd8a9733
grmsoft <grmsoft@mail.ru>
2022-02-02 14:58:59 +0300

    Update ./init.sh add django db migrations

--- init.sh
+++ init.sh
@@ -9,3 +9,14 @@ sudo /etc/init.d/nginx restart
 sudo gunicorn -b "0.0.0.0:8080" hello:print_query &
 cd ask/
 sudo gunicorn -b "0.0.0.0:8000" ask.wsgi:application &
+cd ..
+
+# mysql
+sudo mysql -uroot -e "CREATE DATABASE 'stepic_course_mail_ru';"
+sudo mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'xob01)!';"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON 'stepic_course_mail_ru'.* TO 'box'@'localhost';"
+
+# django db
+cd ask/
+sudo python3 manage.py makemigrations
+sudo python3 manage.py migrate
