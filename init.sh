0b5d5439651045bda8ca2f66a0deb6c0a3bcacd1
Aidar1435 <101663549+Aidar1435@users.noreply.github.com>
2022-03-15 23:49:45 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
 
 
-# nginx
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-
-# mysql # disable, using default sqlite3
-#sudo /etc/init.d/mysql start
-#sudo mysql -u root -e "CREATE DATABASE stepik_course_mail_ru;"
-#sudo mysql -u root -e "CREATE USER box@'%' IDENTIFIED BY 'box';"
-#sudo mysql -u root -e "GRANT ALL PRIVILEGES ON stepic_course_mail_ru.* TO box@'%' WITH GRANT OPTION;"
-#sudo mysql -u root -e "FLUSH PRIVILEGES;
+mysql # disable, using default sqlite3
+sudo /etc/init.d/mysql start
+sudo mysql -u root -e "CREATE DATABASE stepik_course_mail_ru;"
+sudo mysql -u root -e "CREATE USER box@'%' IDENTIFIED BY 'box';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON stepic_course_mail_ru.* TO box@'%' WITH GRANT OPTION;"
+sudo mysql -u root -e "FLUSH PRIVILEGES;
 
 cd ask/
 sudo python3 manage.py makemigrations
